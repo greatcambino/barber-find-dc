7885a824d9574ee8defec598a1b2c99a2d14f6d6
Zach Bauer <zbauer91@gmail.com>
2018-01-30 15:17:38 -0500

    added demo state and handed down props to ListItemView

--- src/components/ListViewItem/ListViewItem.js
+++ src/components/ListViewItem/ListViewItem.js
@@ -2,40 +2,26 @@ import React, { Component } from 'react'
 import { List, Card } from 'semantic-ui-react'
 
 class ListViewItem extends Component {
-  state = {
-    shops: [{
-      name: 'demo cuts',
-      location: '123 demo street, Washington, D.C., 20003',
-      phone: '123-456-7890'
-		},
-		{
-      name: 'demo cuts 2',
-      location: '6129 Grey Friar Way, Dublin, OH, 43017',
-      phone: '102-443-1230'
-		},
-		{
-      name: 'demo cuts 3',
-      location: '1134 demo street, Seattle, WA, 12345',
-      phone: '123-456-7890'
-    }]
-  }
-
   render = () => {
     return (
-			this.props.businesses.map((item, index) => {
-				return (
-					<List.Item>
-						<Card> 
-							<Card.Content>
-								<Card.Header>{item.name}</Card.Header>
-								<Card.Meta>{item.location}</Card.Meta>
-								<Card.Meta>{item.phone}</Card.Meta>
-								<Card.Content>{}</Card.Content>
-							</Card.Content>
-						</Card>
-					</List.item>
-				)
-			})
+      <List.Item>
+        <Card>
+          <Card.Content>
+            <Card.Header>
+              {this.props.business.name}
+            </Card.Header>
+            <Card.Meta>
+              {this.props.business.location}
+            </Card.Meta>
+            <Card.Meta>
+              {this.props.business.phone}
+            </Card.Meta>
+            <Card.Content>
+              {this.props.business.topRating}
+            </Card.Content>
+          </Card.Content>
+        </Card>
+      </List.Item>
     )
   }
 }
--- src/pages/ListView/ListView.js
+++ src/pages/ListView/ListView.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react'
+import { List } from 'semantic-ui-react'
 import NavBar from '../../components/NavBar/NavBar'
 import Footer from '../../components/Footer/Footer'
+import ListViewItem from '../../components/ListViewItem/ListViewItem'
 
 class ListView extends Component {
   state = {
-    currentYear: ''
+    currentYear: '',
+    shops: [
+      {
+        name: 'demo cuts',
+        location: '123 demo street, Washington, D.C., 20003',
+        phone: '123-456-7890',
+        topRating:
+          'This is where a top rating would go, if we had an example to give...'
+      },
+      {
+        name: 'demo cuts 2',
+        location: '6129 Grey Friar Way, Dublin, OH, 43017',
+        phone: '102-443-1230',
+        topRating:
+          'This is where a top rating would go, if we had an example to give...'
+      },
+      {
+        name: 'demo cuts 3',
+        location: '1134 demo street, Seattle, WA, 12345',
+        phone: '123-456-7890',
+        topRating:
+          'This is where a top rating would go, if we had an example to give...'
+      }
+    ]
   }
 
   componentDidMount = () => {
@@ -18,6 +43,11 @@ class ListView extends Component {
     return (
       <div>
         <NavBar />
+        <List>
+          {this.state.shops.map((item, idx) => {
+            return <ListViewItem business={item} />
+          })}
+        </List>
         <Footer year={this.state.currentYear} />
       </div>
     )
