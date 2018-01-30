59d2fab278430e46029df8d77e27ae9d5910fff3
TERRENCE CAMPBELL <TLCAMPBELL89@GMAIL.COM>
2018-01-29 21:58:28 -0500

    tweaked welcome copy

--- src/components/Welcomedialog/Welcomedialog.js
+++ src/components/Welcomedialog/Welcomedialog.js
@@ -9,7 +9,7 @@ export default class WelcomeDialog extends Component {
              <Modal open={true} dimmer={false} size='small'> 
                 <Modal.Header className='header'>BarberHub</Modal.Header>
                 <Modal.Content className='description'>
-                    <p>Whether you’re looking for your next barber or looking for better tips on cutting your own hair, this app is a one-stop shop. BarberHub lists highly rated barbershops in the Washington DC area along with a round-up of tutorials in self-barbering. Our team was given exactly one week to build out this app, which incorporates MongoDB, Express, React.js, and API integration. Click one of the icons below to begin!</p>
+                    <p>Whether you’re looking for your next barber or looking for better tips on cutting your own hair, this app is a one-stop shop. BarberHub lists highly rated barbershops in the Washington DC area along with a round-up of tutorials in self-barbering. This app incorporates MongoDB, Express, React.js, and API integration. Click one of the icons below to begin!</p>
                 </Modal.Content>
                 <Modal.Actions className='button-container'>
                     <Button className='button-icons'>
